5af09a6b0abfe1fba3e46147f9139ac8f6ea72b9
Matias Hernandez <matias2k00@gmail.com>
2023-07-20 14:10:36 -0300

    api/src/routes/historyShop/index.ts
    ruta de history shop

--- api/src/lib/database/models/Product.ts
+++ api/src/lib/database/models/Product.ts
@@ -1,55 +1,91 @@
-//! MODELO DE Product 
+//! MODELO DE Product
 
-import { User } from "./User";
-import { Model, Column, Table, CreatedAt, UpdatedAt, BelongsTo, ForeignKey } from "sequelize-typescript";
+import { User } from './User';
+import {
+  Model,
+  Column,
+  Table,
+  CreatedAt,
+  UpdatedAt,
+  BelongsTo,
+  ForeignKey,
+  DataType,
+} from 'sequelize-typescript';
 
-interface ProductModel {
-    id: number,
-    amount: number,
-    name: string,
-    description: string,
-    createdAt: Date,
-    updatedAt: Date,
+export interface ProductModel {
+  id: number;
+  image: string;
+  price: number;
+  name: string;
+  description: string;
+  color: string;
+  brand: string;
+  category: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
 @Table({
-    timestamps: true,
-    tableName:"Product",
-    freezeTableName: true,
-    modelName:"Product",
+  timestamps: true,
+  tableName: 'Product',
+  freezeTableName: true,
+  modelName: 'Product',
 })
 export class Product extends Model<ProductModel> {
-    @Column({
-        primaryKey: true,
-        autoIncrement: true,
-    })
-    id!: number;
+  @Column({
+    primaryKey: true,
+    autoIncrement: true,
+  })
+  id!: number;
 
-    @Column({
-        allowNull: false
-    })
-    amount!: number
+  @Column({
+    allowNull: false,
+  })
+  price!: number;
 
-    @Column({
-        allowNull: false
-    })
-    name!: string
+  @Column({
+    allowNull: false,
+  })
+  name!: string;
 
-    @Column({
-        allowNull: true
-    })
-    description!: string
+  @Column({
+    allowNull: false,
+  })
+  category!: string;
 
-    @CreatedAt
-    @Column
-    createdAt!: Date;
+  @Column({
+    allowNull: true,
+    defaultValue: '',
+  })
+  image!: string;
 
-    @UpdatedAt
-    @Column
-    updatedAt!: Date;
+  @Column({
+    allowNull: true,
+    type: DataType.TEXT,
+  })
+  description!: string;
 
-    @ForeignKey(() => User)
-    @Column
-    productId!: number
-    @BelongsTo(() => User)
-    user!: User
+  @Column({
+    allowNull: false,
+  })
+  color!: string;
+
+  @Column({
+    allowNull: false,
+  })
+  brand!: string;
+
+  @CreatedAt
+  @Column
+  createdAt!: Date;
+
+  @UpdatedAt
+  @Column
+  updatedAt!: Date;
+
+  @ForeignKey(() => User)
+  @Column
+  productId!: number;
+
+  @BelongsTo(() => User)
+  user!: User;
 }
