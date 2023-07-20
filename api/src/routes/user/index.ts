43d3902414c386ce3d3de2f93bf56e95801b5502
Matias Hernandez <matias2k00@gmail.com>
2023-07-19 22:39:57 -0300

    rout user add

--- api/src/routes/user/index.ts
+++ api/src/routes/user/index.ts
@@ -1,12 +1,18 @@
 //! rutas de users
 import { Router } from 'express';
-import { allUsers, oneUser, postUser } from '../../controllers/Users/index';
+import {
+  allUsers,
+  oneUser,
+  postUser,
+  logUser,
+} from '../../controllers/Users/index';
 import { Request, Response } from 'express';
 
 const userRouter: Router = Router();
 
 userRouter.post('/', postUser);
 userRouter.get('/all', allUsers);
-userRouter.get('/:id', oneUser)
+userRouter.get('/:id', oneUser);
+userRouter.post('/login', logUser);
 
 export default userRouter;
