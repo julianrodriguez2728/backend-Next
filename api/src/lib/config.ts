3b53c24bccca36bcff2c998d30ce133b33e6455a
Lucas52352 <lucasrius86@gmail.com>
2023-07-20 09:30:30 -0300

    .gitignored created

--- api/src/lib/config.ts
+++ api/src/lib/config.ts
@@ -2,9 +2,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-  dbUser: process.env.DB_USER || 'postgres',
+  dbUser: process.env.DB_USER || 'mrguiso',
   dbName: process.env.DB_NAME || 'gaming',
-  dbPassword: process.env.DB_PASSWORD || '44724861dni',
+  dbPassword: process.env.DB_PASSWORD || '602223Lucas.',
   dbHost: process.env.DB_HOST || 'localhost',
   dbPort: process.env.DB_PORT || '3001',
 };
