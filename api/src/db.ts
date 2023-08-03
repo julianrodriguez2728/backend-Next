e5a739dba9dd98489f9280a4753c043d4ca07304
julianrodriguez2728 <julirdrg@gmail.com>
2023-08-03 12:31:30 -0300

    vercel

--- api/src/db.ts
+++ api/src/db.ts
@@ -5,10 +5,24 @@ import { Sequelize } from "sequelize-typescript";
 import { User } from "./lib/database/models/User";
 import { Product } from "./lib/database/models/Product";
 
-export const sequelize = new Sequelize({
-  dialect: 'postgres',
-  database: config.dbName,
-  password: config.dbPassword,
-  username: config.dbUser,
-  models: [User, Product],
-});
+import pg from 'pg';
+
+
+const { Pool } = pg;
+
+
+const pool = new Pool({
+
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+
+})
+
+// export const sequelize = new Sequelize({
+//   dialect: 'postgres',
+//   database: config.dbName,
+//   password: config.dbPassword,
+//   username: config.dbUser,
+//   models: [User, Product],
+// });
+
+module.exports = pool
